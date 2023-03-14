8554cce5fb802aa1f6fa2ca9f89482647b005adf
jeremy.zhang <zhangyue9306@gmail.com>
2023-03-14 11:15:14 +0800

    add flow

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, request
+# from flask import Flask, request
+#
+# app = Flask(__name__)
+#
+# # TODO try flask
+#
+# @app.route('/api/v1/submit', methods=['POST'])
+# def submit_data():
+#     data = request.get_json()
+#     return 'Data received: {}'.format(data)
+#
+#
+# if __name__ == '__main__':
+#     app.run(port=3000)
+#
+#
+import os
+from slack_bolt import App
 
-app = Flask(__name__)
-
-# TODO follow up the bot example
-
-@app.route('/api/v1/submit', methods=['POST'])
-def submit_data():
-    data = request.get_json()
-    return 'Data received: {}'.format(data)
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
 
 
-if __name__ == '__main__':
-    app.run(port=3000)
+@app.message("hello")
+def echo(message, say):
+    print(message)
+    say(f"Yes <@{message['user']}>")
+
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
